7abd4c537dd839494a4927fab940effac66913a8
Sou <suta1218@yahoo.co.jp>
2016-07-10 18:13:46 +0900

    m n o

---
+++ .metadata/.plugins/com.aptana.ruby.core/-1056291454/4/etc.rb
@@ -0,0 +1,282 @@
+module Etc
+
+  CS_PATH = 1
+  CS_POSIX_V6_ILP32_OFF32_CFLAGS = 2
+  CS_POSIX_V6_ILP32_OFF32_LDFLAGS = 3
+  CS_POSIX_V6_ILP32_OFF32_LIBS = 4
+  CS_POSIX_V6_ILP32_OFFBIG_CFLAGS = 5
+  CS_POSIX_V6_ILP32_OFFBIG_LDFLAGS = 6
+  CS_POSIX_V6_ILP32_OFFBIG_LIBS = 7
+  CS_POSIX_V6_LP64_OFF64_CFLAGS = 8
+  CS_POSIX_V6_LP64_OFF64_LDFLAGS = 9
+  CS_POSIX_V6_LP64_OFF64_LIBS = 10
+  CS_POSIX_V6_LPBIG_OFFBIG_CFLAGS = 11
+  CS_POSIX_V6_LPBIG_OFFBIG_LDFLAGS = 12
+  CS_POSIX_V6_LPBIG_OFFBIG_LIBS = 13
+  CS_POSIX_V6_WIDTH_RESTRICTED_ENVS = 14
+  Group = Etc::Group
+  PC_2_SYMLINKS = 15
+  PC_ALLOC_SIZE_MIN = 16
+  PC_ASYNC_IO = 17
+  PC_CHOWN_RESTRICTED = 7
+  PC_FILESIZEBITS = 18
+  PC_LINK_MAX = 1
+  PC_MAX_CANON = 2
+  PC_MAX_INPUT = 3
+  PC_NAME_MAX = 4
+  PC_NO_TRUNC = 8
+  PC_PATH_MAX = 5
+  PC_PIPE_BUF = 6
+  PC_PRIO_IO = 19
+  PC_REC_INCR_XFER_SIZE = 20
+  PC_REC_MAX_XFER_SIZE = 21
+  PC_REC_MIN_XFER_SIZE = 22
+  PC_REC_XFER_ALIGN = 23
+  PC_SYMLINK_MAX = 24
+  PC_SYNC_IO = 25
+  PC_VDISABLE = 9
+  Passwd = Etc::Passwd
+  SC_2_CHAR_TERM = 20
+  SC_2_C_BIND = 18
+  SC_2_C_DEV = 19
+  SC_2_FORT_DEV = 21
+  SC_2_FORT_RUN = 22
+  SC_2_LOCALEDEF = 23
+  SC_2_PBS = 59
+  SC_2_PBS_ACCOUNTING = 60
+  SC_2_PBS_CHECKPOINT = 61
+  SC_2_PBS_LOCATE = 62
+  SC_2_PBS_MESSAGE = 63
+  SC_2_PBS_TRACK = 64
+  SC_2_SW_DEV = 24
+  SC_2_UPE = 25
+  SC_2_VERSION = 17
+  SC_ADVISORY_INFO = 65
+  SC_AIO_LISTIO_MAX = 42
+  SC_AIO_MAX = 43
+  SC_AIO_PRIO_DELTA_MAX = 44
+  SC_ARG_MAX = 1
+  SC_ASYNCHRONOUS_IO = 28
+  SC_ATEXIT_MAX = 107
+  SC_BARRIERS = 66
+  SC_BC_BASE_MAX = 9
+  SC_BC_DIM_MAX = 10
+  SC_BC_SCALE_MAX = 11
+  SC_BC_STRING_MAX = 12
+  SC_CHILD_MAX = 2
+  SC_CLK_TCK = 3
+  SC_CLOCK_SELECTION = 67
+  SC_COLL_WEIGHTS_MAX = 13
+  SC_CPUTIME = 68
+  SC_DELAYTIMER_MAX = 45
+  SC_EXPR_NEST_MAX = 14
+  SC_FSYNC = 38
+  SC_GETGR_R_SIZE_MAX = 70
+  SC_GETPW_R_SIZE_MAX = 71
+  SC_HOST_NAME_MAX = 72
+  SC_IOV_MAX = 56
+  SC_IPV6 = 118
+  SC_JOB_CONTROL = 6
+  SC_LINE_MAX = 15
+  SC_LOGIN_NAME_MAX = 73
+  SC_MAPPED_FILES = 47
+  SC_MEMLOCK = 30
+  SC_MEMLOCK_RANGE = 31
+  SC_MEMORY_PROTECTION = 32
+  SC_MESSAGE_PASSING = 33
+  SC_MONOTONIC_CLOCK = 74
+  SC_MQ_OPEN_MAX = 46
+  SC_MQ_PRIO_MAX = 75
+  SC_NGROUPS_MAX = 4
+  SC_NPROCESSORS_CONF = 57
+  SC_NPROCESSORS_ONLN = 58
+  SC_OPEN_MAX = 5
+  SC_PAGESIZE = 29
+  SC_PAGE_SIZE = 29
+  SC_PHYS_PAGES = 200
+  SC_PRIORITIZED_IO = 34
+  SC_PRIORITY_SCHEDULING = 35
+  SC_RAW_SOCKETS = 119
+  SC_READER_WRITER_LOCKS = 76
+  SC_REALTIME_SIGNALS = 36
+  SC_REGEXP = 77
+  SC_RE_DUP_MAX = 16
+  SC_RTSIG_MAX = 48
+  SC_SAVED_IDS = 7
+  SC_SEMAPHORES = 37
+  SC_SEM_NSEMS_MAX = 49
+  SC_SEM_VALUE_MAX = 50
+  SC_SHARED_MEMORY_OBJECTS = 39
+  SC_SHELL = 78
+  SC_SIGQUEUE_MAX = 51
+  SC_SPAWN = 79
+  SC_SPIN_LOCKS = 80
+  SC_SPORADIC_SERVER = 81
+  SC_SS_REPL_MAX = 126
+  SC_STREAM_MAX = 26
+  SC_SYMLOOP_MAX = 120
+  SC_SYNCHRONIZED_IO = 40
+  SC_THREADS = 96
+  SC_THREAD_ATTR_STACKADDR = 82
+  SC_THREAD_ATTR_STACKSIZE = 83
+  SC_THREAD_CPUTIME = 84
+  SC_THREAD_DESTRUCTOR_ITERATIONS = 85
+  SC_THREAD_KEYS_MAX = 86
+  SC_THREAD_PRIORITY_SCHEDULING = 89
+  SC_THREAD_PRIO_INHERIT = 87
+  SC_THREAD_PRIO_PROTECT = 88
+  SC_THREAD_PROCESS_SHARED = 90
+  SC_THREAD_SAFE_FUNCTIONS = 91
+  SC_THREAD_SPORADIC_SERVER = 92
+  SC_THREAD_STACK_MIN = 93
+  SC_THREAD_THREADS_MAX = 94
+  SC_TIMEOUTS = 95
+  SC_TIMERS = 41
+  SC_TIMER_MAX = 52
+  SC_TRACE = 97
+  SC_TRACE_EVENT_FILTER = 98
+  SC_TRACE_EVENT_NAME_MAX = 127
+  SC_TRACE_INHERIT = 99
+  SC_TRACE_LOG = 100
+  SC_TRACE_NAME_MAX = 128
+  SC_TRACE_SYS_MAX = 129
+  SC_TRACE_USER_EVENT_MAX = 130
+  SC_TTY_NAME_MAX = 101
+  SC_TYPED_MEMORY_OBJECTS = 102
+  SC_TZNAME_MAX = 27
+  SC_V6_ILP32_OFF32 = 103
+  SC_V6_ILP32_OFFBIG = 104
+  SC_V6_LP64_OFF64 = 105
+  SC_V6_LPBIG_OFFBIG = 106
+  SC_VERSION = 8
+  SC_XOPEN_CRYPT = 108
+  SC_XOPEN_ENH_I18N = 109
+  SC_XOPEN_REALTIME = 111
+  SC_XOPEN_REALTIME_THREADS = 112
+  SC_XOPEN_SHM = 113
+  SC_XOPEN_STREAMS = 114
+  SC_XOPEN_UNIX = 115
+  SC_XOPEN_VERSION = 116
+
+  def self.confstr(arg0)
+  end
+
+  def self.endgrent
+  end
+
+  def self.endpwent
+  end
+
+  def self.getgrent
+  end
+
+  def self.getgrgid(arg0, arg1, *rest)
+  end
+
+  def self.getgrnam(arg0)
+  end
+
+  def self.getlogin
+  end
+
+  def self.getpwent
+  end
+
+  def self.getpwnam(arg0)
+  end
+
+  def self.getpwuid(arg0, arg1, *rest)
+  end
+
+  def self.group
+  end
+
+  def self.nprocessors
+  end
+
+  def self.passwd
+  end
+
+  def self.setgrent
+  end
+
+  def self.setpwent
+  end
+
+  def self.sysconf(arg0)
+  end
+
+  def self.sysconfdir
+  end
+
+  def self.systmpdir
+  end
+
+  def self.uname
+  end
+
+
+
+  protected
+
+
+  private
+
+  def confstr(arg0)
+  end
+
+  def endgrent
+  end
+
+  def endpwent
+  end
+
+  def getgrent
+  end
+
+  def getgrgid(arg0, arg1, *rest)
+  end
+
+  def getgrnam(arg0)
+  end
+
+  def getlogin
+  end
+
+  def getpwent
+  end
+
+  def getpwnam(arg0)
+  end
+
+  def getpwuid(arg0, arg1, *rest)
+  end
+
+  def group
+  end
+
+  def nprocessors
+  end
+
+  def passwd
+  end
+
+  def setgrent
+  end
+
+  def setpwent
+  end
+
+  def sysconf(arg0)
+  end
+
+  def sysconfdir
+  end
+
+  def systmpdir
+  end
+
+  def uname
+  end
+
+end
---
+++ .metadata/.plugins/com.aptana.ruby.core/-1056291454/4/object.rb
@@ -0,0 +1,193 @@
+class Object < BasicObject
+  include Kernel
+
+  ARGF = ARGF
+  ARGV = []
+  Addrinfo = Addrinfo
+  ArgumentError = ArgumentError
+  Array = Array
+  BasicObject = BasicObject
+  BasicSocket = BasicSocket
+  Bignum = Bignum
+  Binding = Binding
+  CGI = CGI
+  CROSS_COMPILING = nil
+  Class = Class
+  Comparable = Comparable
+  Complex = Complex
+  ConditionVariable = Thread::ConditionVariable
+  Data = Data
+  Date = Date
+  DateTime = DateTime
+  Delegator = Delegator
+  Digest = Digest
+  Dir = Dir
+  ENV = {"SHELL"=>"/bin/bash", "JAVA_STARTED_ON_FIRST_THREAD_1029"=>"1", "TMPDIR"=>"/var/folders/yz/sjnqhp0d3lv2jr55ycm5tfrr0000gn/T/", "SHLVL"=>"1", "__CF_USER_TEXT_ENCODING"=>"0x1F5:0x1:0xE", "PATH"=>"/Users/HondaSoichiro/.rbenv/shims:/Users/HondaSoichiro/.rbenv/shims:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin", "XPC_FLAGS"=>"0x0", "APTANA_VERSION"=>"3.6.2.1413590556", "JAVA_STARTED_ON_FIRST_THREAD_3883"=>"1", "USER"=>"HondaSoichiro", "PWD"=>"/Applications/Aptana Studio 3/AptanaStudio3.app/Contents/MacOS", "RBENV_SHELL"=>"bash", "com.apple.java.jvmTask"=>"JNI", "HOME"=>"/Users/HondaSoichiro", "APP_ICON_3883"=>"../Resources/aptana.icns", "XPC_SERVICE_NAME"=>"0", "LOGNAME"=>"HondaSoichiro", "Apple_PubSub_Socket_Render"=>"/private/tmp/com.apple.launchd.ImYQ9YNsE7/Render", "APP_ICON_1029"=>"../Resources/aptana.icns", "SSH_AUTH_SOCK"=>"/private/tmp/com.apple.launchd.xuulVe51uS/Listeners", "LANG"=>"en_US.UTF-8"}
+  EOFError = EOFError
+  Encoding = Encoding
+  EncodingError = EncodingError
+  Enumerable = Enumerable
+  Enumerator = Enumerator
+  Errno = Errno
+  Etc = Etc
+  Exception = Exception
+  FALSE = false
+  FalseClass = FalseClass
+  Fcntl = Fcntl
+  Fiber = Fiber
+  FiberError = FiberError
+  File = File
+  FileTest = FileTest
+  FileUtils = FileUtils
+  Fixnum = Fixnum
+  Float = Float
+  FloatDomainError = FloatDomainError
+  GC = GC
+  Gem = Gem
+  Hash = Hash
+  IO = IO
+  IOError = IOError
+  IPSocket = IPSocket
+  IndexError = IndexError
+  Integer = Integer
+  Interrupt = Interrupt
+  Kernel = Kernel
+  KeyError = KeyError
+  LoadError = LoadError
+  LocalJumpError = LocalJumpError
+  Marshal = Marshal
+  MatchData = MatchData
+  Math = Math
+  Method = Method
+  Module = Module
+  Monitor = Monitor
+  MonitorMixin = MonitorMixin
+  Mutex = Mutex
+  NIL = nil
+  NameError = NameError
+  Net = Net
+  NilClass = NilClass
+  NoMemoryError = NoMemoryError
+  NoMethodError = NoMethodError
+  NotImplementedError = NotImplementedError
+  Numeric = Numeric
+  OUTPUT_PATH = "/Users/HondaSoichiro/Desktop/TSM/.metadata/.plugins/com.aptana.ruby.core/-1056291454/4/"
+  Object = Object
+  ObjectSpace = ObjectSpace
+  OpenSSL = OpenSSL
+  OptParse = OptionParser
+  OptionParser = OptionParser
+  Proc = Proc
+  Process = Process
+  Queue = Thread::Queue
+  RUBY_COPYRIGHT = "ruby - Copyright (C) 1993-2015 Yukihiro Matsumoto"
+  RUBY_DESCRIPTION = "ruby 2.2.2p95 (2015-04-13 revision 50295) [x86_64-darwin15]"
+  RUBY_ENGINE = "ruby"
+  RUBY_PATCHLEVEL = 95
+  RUBY_PLATFORM = "x86_64-darwin15"
+  RUBY_RELEASE_DATE = "2015-04-13"
+  RUBY_REVISION = 50295
+  RUBY_VERSION = "2.2.2"
+  Random = Random
+  Range = Range
+  RangeError = RangeError
+  Rational = Rational
+  RbConfig = RbConfig
+  Regexp = Regexp
+  RegexpError = RegexpError
+  Resolv = Resolv
+  RubyVM = RubyVM
+  RuntimeError = RuntimeError
+  STDERR = IO.new
+  STDIN = IO.new
+  STDOUT = IO.new
+  ScanError = StringScanner::Error
+  ScriptError = ScriptError
+  SecureRandom = SecureRandom
+  SecurityError = SecurityError
+  Signal = Signal
+  SignalException = SignalException
+  SimpleDelegator = SimpleDelegator
+  SizedQueue = Thread::SizedQueue
+  Socket = Socket
+  SocketError = SocketError
+  StandardError = StandardError
+  StopIteration = StopIteration
+  String = String
+  StringIO = StringIO
+  StringScanner = StringScanner
+  Struct = Struct
+  Symbol = Symbol
+  SyntaxError = SyntaxError
+  SystemCallError = SystemCallError
+  SystemExit = SystemExit
+  SystemStackError = SystemStackError
+  TCPServer = TCPServer
+  TCPSocket = TCPSocket
+  TOPLEVEL_BINDING = #<Binding:0x007fa07b868350>
+  TRUE = true
+  TSort = TSort
+  Tempfile = Tempfile
+  Thread = Thread
+  ThreadError = ThreadError
+  ThreadGroup = ThreadGroup
+  Time = Time
+  Timeout = Timeout
+  TimeoutError = Timeout::Error
+  TracePoint = TracePoint
+  TrueClass = TrueClass
+  TypeError = TypeError
+  UDPSocket = UDPSocket
+  UNIXServer = UNIXServer
+  UNIXSocket = UNIXSocket
+  URI = URI
+  UnboundMethod = UnboundMethod
+  UncaughtThrowError = UncaughtThrowError
+  ZeroDivisionError = ZeroDivisionError
+  Zlib = Zlib
+  fatal = nil
+
+
+
+  protected
+
+
+  private
+
+  def DelegateClass(arg0)
+  end
+
+  def Digest(arg0)
+  end
+
+  def dir_names(arg0)
+  end
+
+  def file_name(arg0)
+  end
+
+  def get_classes
+  end
+
+  def grab_instance_method(arg0, arg1)
+  end
+
+  def print_args(arg0)
+  end
+
+  def print_instance_method(arg0, arg1)
+  end
+
+  def print_method(arg0, arg1, arg2, arg3, arg4, *rest)
+  end
+
+  def print_type(arg0)
+  end
+
+  def print_value(arg0)
+  end
+
+  def timeout(arg0, arg1, arg2, *rest)
+  end
+
+end
