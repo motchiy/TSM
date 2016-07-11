728284169b4d4b4b2f8c18640ad68d5aa231f937
Sou <suta1218@yahoo.co.jp>
2016-07-11 22:52:27 +0900

    sou

---
+++ petch/app/controllers/notes_controller.rb
@@ -0,0 +1,39 @@
+class NotesController < ApplicationController
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @note = Note.new
+    @note.title = params[:title]
+    @note.content = params[:content]
+    @note.save
+    redirect_to note_path(@note.id)
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    @note.title = params[:title]
+    @note.content = params[:content]
+    @note.save
+    redirect_to note_path(@note.id)
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to notes_path
+  end
+end
